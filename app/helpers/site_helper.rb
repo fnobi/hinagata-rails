93c0623a48b10e8357ff9210ffcac242ceca56a7
fujisawa-shin <fujisawa-shin@kayac.com>
2015-12-27 16:08:22 +0900

    meta tagsの追加

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,42 @@
 module SiteHelper
+  def default_meta_tags
+    {
+      site: Settings.meta_tags.site_name,
+      reverse: true,
+      description: Settings.meta_tags.description,
+      keywords: Settings.meta_tags.keywords,
+      icon: [
+        { href: '/favicon.ico', },
+        {
+          href: "#{root_url}homeicon.png",
+          rel: 'apple-touch-icon-precomposed',
+          type: 'image/png'
+        },
+      ],
+      og: {
+        title: :title,
+        type: "website",
+        image: {
+          _: "#{root_url}ogp.jpg",
+          width: 1200,
+          height: 630
+        },
+        site_name: Settings.meta_tags.site_name,
+        description: :description,
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: :title,
+        description: :description,
+        image: {
+          _: "#{root_url}ogp.jpg",
+          width: 1200,
+          height: 630
+        },
+        site: Settings.meta_tags.twitter_account,
+        creator: Settings.meta_tags.twitter_account
+      }
+    }
+  end
 end
