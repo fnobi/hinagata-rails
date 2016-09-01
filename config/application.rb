f686181e171406f7b3426703a60e06acd3a6a193
fujisawa-shin <fujisawa-shin@kayac.com>
2016-09-01 17:29:05 +0900

    と見せかけて削除

--- config/application.rb
+++ config/application.rb
@@ -25,10 +25,6 @@ module HinagataRails
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # browserify options
-    config.browserify_rails.use_browserifyinc = true
-    config.browserify_rails.commandline_options = '-t [ babelify --presets [ es2015 ] ]'
-
     config.generators do |g|
       g.javascripts false
       g.stylesheets false
